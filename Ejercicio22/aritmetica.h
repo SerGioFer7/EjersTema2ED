7ca5497f2899286c47d0a962e69c77c4bec49f47
SerGioFer7 <91659108+SerGioFer7@users.noreply.github.com>
2021-12-01 22:30:34 +0100

    Delete Ejercicio22 directory

--- Ejercicio22/aritmetica.h
+++
@@ -1,7 +0,0 @@
-int suma (int sumando1, int sumando2);
-
-int resta  (int minuendo, int sustraendo);
-
-int multiplicacion (int  numero1, int numero2);
-
-float division (int dividendo, int divisor);
